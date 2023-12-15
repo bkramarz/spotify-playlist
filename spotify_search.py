a16b11c564f3ab41ca5e0c2409bc4d3c4b24e38b
bkramarz <105524805+bkramarz@users.noreply.github.com>
2023-12-15 10:36:14 -0800

    Add files via upload

---
+++ spotify_search.py
@@ -0,0 +1,69 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+
+os.environ["SPOTIPY_CLIENT_ID"] = "63a4bee8ef494519b4b94df8a05aa3e2"
+os.environ["SPOTIFY_CLIENT_SECRET"] = "0100d66e1c9a4df98fd2535262602c86"
+
+# Available scopes:
+
+# Images
+# ugc-image-upload
+
+# Spotify Connect
+# user-read-playback-state
+# user-modify-playback-state
+# user-read-currently-playing
+
+# Playback
+# app-remote-control
+# streaming
+
+# Playlists
+# playlist-read-private
+# playlist-read-collaborative
+# playlist-modify-private
+# playlist-modify-public
+
+# Follow
+# user-follow-modify
+# user-follow-read
+
+# Listening History
+# user-read-playback-position
+# user-top-read
+# user-read-recently-played
+
+# Library
+# user-library-modify
+# user-library-read
+
+# Users
+# user-read-email
+# user-read-private
+
+
+class SpotifySearch(spotipy.Spotify):
+
+    def __init__(self , scope):
+        super().__init__()
+        self.scope = scope
+        self.auth_manager = SpotifyOAuth(scope=self.scope,
+                                         client_secret=os.environ.get("SPOTIFY_CLIENT_SECRET"),
+                                         client_id=os.environ.get("SPOTIPY_CLIENT_ID"),
+                                         redirect_uri="https://localhost:8888/callback")
+
+    def get_artist_uri(self, artist_name):
+        return self.search(q=f"artist: {artist_name}")["tracks"]["items"][0]["artists"][0]["uri"]
+
+    def get_song_uri(self, song_name):
+        return self.search(q=f"song: {song_name}")["tracks"]["items"][0]["uri"]
+
+    def get_related_artists(self, artist_name):
+        artist_uri = self.get_artist_uri(artist_name)
+        related_artists = [artist["name"] for artist in self.artist_related_artists(artist_uri)["artists"]]
+        return related_artists
+
+    def get_top_tracks(self, artist_name):
+        top_tracks = [track["name"] for track in self.artist_top_tracks(self.get_artist_uri(artist_name), country='US')["tracks"]]
+        return top_tracks
---
+++ wed_night_jam_playlist_creator.py
@@ -0,0 +1,38 @@
+import pandas
+from spotify_search import SpotifySearch
+
+# Instantiate SpotifySearch object
+
+sp = SpotifySearch()
+
+# Get user input date
+
+date = input("Enter jam date (M_D_YY): ")
+
+# Open csv and put songs into dictionary
+
+with open(f"song_lists/Wednesday night Jam songs - {date}.csv") as file:
+    df = pandas.read_csv(file)
+    song_dict = df.to_dict('records')
+    print(song_dict)
+
+# Create URI list
+
+uri_list = []
+for song in song_dict:
+    try:
+        uri = sp.search(q=f"track: {song['title']}, artist: {song['artist']}")["tracks"]["items"][0]["uri"]
+        uri_list.append(uri)
+    except IndexError:
+        pass
+
+# Create playlist and add songs
+
+playlist_create = sp.user_playlist_create(
+    user="benkramarz",
+    name=f"{date} KK jam",
+    public=True,
+    collaborative=False,
+    description='')
+
+playlist_add = sp.playlist_add_items(playlist_id=playlist_create['id'], items=uri_list, position=None)
